e7d9eb94ed7876c2e9b8e80791077b59d1f727dd
Christopher Kolstad <chriswk@getunleash.io>
2023-03-08 13:16:43 +0100

    chore: Added tests for edge and client_api
    Also added some Tarpaulin exclusion arguments to better reflect code
    coverage across code that we actually want to cover.
    
    There's still some work to do with regards to having the possibility to
    instantiate the entire application as a test service for full
    integration/e2e tests, but this at least covers more logic and also
    exposed a bug in how we validated keys when not using a token validator
    (offline mode))

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -1,19 +1,24 @@
-pub mod metrics;
-pub mod prom_metrics;
-
 pub mod auth;
+#[cfg(not(tarpaulin_include))]
 pub mod builder;
+#[cfg(not(tarpaulin_include))]
 pub mod cli;
 pub mod client_api;
 pub mod edge_api;
+#[cfg(not(tarpaulin_include))]
 pub mod error;
 pub mod frontend_api;
 pub mod http;
+pub mod internal_backstage;
+pub mod metrics;
 pub mod middleware;
+#[cfg(not(tarpaulin_include))]
 pub mod openapi;
 pub mod persistence;
+#[cfg(not(tarpaulin_include))]
+pub mod prom_metrics;
+#[cfg(not(tarpaulin_include))]
+pub mod tls;
 pub mod tokens;
 pub mod types;
 pub mod urls;
-
-pub mod internal_backstage;
