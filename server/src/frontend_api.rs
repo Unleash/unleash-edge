b3e127a252c27478f39442c9116d72328701d734
Thomas Heartman <thomas@getunleash.io>
2024-03-12 14:28:23 +0100

    Chore: revert changes in frontend_api.rs

--- server/src/frontend_api.rs
+++ server/src/frontend_api.rs
@@ -21,7 +21,7 @@ use unleash_types::{
 use unleash_yggdrasil::{EngineState, ResolvedToggle};
 
 use crate::error::EdgeError::ContextParseError;
-use crate::types::{ClientIp, IncomingContext};
+use crate::types::ClientIp;
 use crate::{
     error::{EdgeError, FrontendHydrationMissing},
     metrics::client_metrics::MetricsCache,
@@ -241,19 +241,18 @@ security(
 )
 )]
 #[get("")]
-#[instrument(skip(edge_token, req, engine_cache, token_cache))]
 async fn get_enabled_proxy(
     edge_token: EdgeToken,
     engine_cache: Data<DashMap<String, EngineState>>,
     token_cache: Data<DashMap<String, EdgeToken>>,
-    context: QsQuery<IncomingContext>,
+    context: QsQuery<Context>,
     req: HttpRequest,
 ) -> EdgeJsonResult<FrontendResult> {
     get_enabled_features(
         edge_token,
         engine_cache,
         token_cache,
-        context.into_inner().into(),
+        context.into_inner(),
         req.extensions().get::<ClientIp>().cloned(),
     )
 }
@@ -276,7 +275,7 @@ async fn get_enabled_frontend(
     edge_token: EdgeToken,
     engine_cache: Data<DashMap<String, EngineState>>,
     token_cache: Data<DashMap<String, EdgeToken>>,
-    context: QsQuery<IncomingContext>,
+    context: QsQuery<Context>,
     req: HttpRequest,
 ) -> EdgeJsonResult<FrontendResult> {
     debug!("getting enabled features");
@@ -285,7 +284,7 @@ async fn get_enabled_frontend(
         edge_token,
         engine_cache,
         token_cache,
-        context.into_inner().into(),
+        context.into_inner(),
         client_ip,
     )
 }
@@ -342,7 +341,7 @@ async fn post_proxy_enabled_features(
     edge_token: EdgeToken,
     engine_cache: Data<DashMap<String, EngineState>>,
     token_cache: Data<DashMap<String, EdgeToken>>,
-    context: Json<IncomingContext>,
+    context: Json<Context>,
     req: HttpRequest,
 ) -> EdgeJsonResult<FrontendResult> {
     let client_ip = req.extensions().get::<ClientIp>().cloned();
@@ -366,7 +365,7 @@ async fn post_frontend_enabled_features(
     edge_token: EdgeToken,
     engine_cache: Data<DashMap<String, EngineState>>,
     token_cache: Data<DashMap<String, EdgeToken>>,
-    context: Json<IncomingContext>,
+    context: Json<Context>,
     req: HttpRequest,
 ) -> EdgeJsonResult<FrontendResult> {
     let client_ip = req.extensions().get::<ClientIp>().cloned();
@@ -493,10 +492,10 @@ async fn post_enabled_features(
     edge_token: EdgeToken,
     engine_cache: Data<DashMap<String, EngineState>>,
     token_cache: Data<DashMap<String, EdgeToken>>,
-    context: Json<IncomingContext>,
+    context: Json<Context>,
     client_ip: Option<ClientIp>,
 ) -> EdgeJsonResult<FrontendResult> {
-    let context: Context = context.into_inner().into();
+    let context = context.into_inner();
     let context_with_ip = if context.remote_address.is_none() {
         Context {
             remote_address: client_ip.map(|ip| ip.to_string()),
@@ -860,34 +859,6 @@ mod tests {
         }
     }
 
-    fn client_features_with_constraint_requiring_test_property_to_be_42() -> ClientFeatures {
-        ClientFeatures {
-            version: 1,
-            features: vec![ClientFeature {
-                name: "test".into(),
-                enabled: true,
-                strategies: Some(vec![Strategy {
-                    name: "default".into(),
-                    sort_order: None,
-                    segments: None,
-                    variants: None,
-                    constraints: Some(vec![Constraint {
-                        context_name: "test_property".into(),
-                        operator: Operator::In,
-                        case_insensitive: false,
-                        inverted: false,
-                        values: Some(vec!["42".into()]),
-                        value: None,
-                    }]),
-                    parameters: None,
-                }]),
-                ..ClientFeature::default()
-            }],
-            segments: None,
-            query: None,
-        }
-    }
-
     fn client_features_with_constraint_one_enabled_toggle_and_one_disabled_toggle() -> ClientFeatures
     {
         ClientFeatures {
@@ -1006,109 +977,6 @@ mod tests {
         };
 
         assert_eq!(result, serde_json::to_vec(&expected).unwrap());
-    }
-
-
-    #[actix_web::test]
-    #[traced_test]
-    async fn calling_get_requests_resolves_top_level_properties_correctly() {
-        let (feature_cache, token_cache, engine_cache) = build_offline_mode(
-            client_features_with_constraint_requiring_test_property_to_be_42(),
-            vec![
-                "*:development.03fa5f506428fe80ed5640c351c7232e38940814d2923b08f5c05fa7"
-                    .to_string(),
-            ],
-        )
-        .unwrap();
-        let app = test::init_service(
-            App::new()
-                .app_data(Data::from(token_cache))
-                .app_data(Data::from(feature_cache))
-                .app_data(Data::from(engine_cache))
-                .service(web::scope("/api/frontend").service(super::get_enabled_frontend))
-                .service(web::scope("/api/proxy").service(super::get_enabled_proxy)),
-        )
-        .await;
-
-        let req = |endpoint| test::TestRequest::get()
-            .uri(format!("/api/{endpoint}?test_property=42").as_str())
-            .insert_header((
-                "Authorization",
-                "*:development.03fa5f506428fe80ed5640c351c7232e38940814d2923b08f5c05fa7",
-            ))
-            .to_request();
-
-        let frontend_result = test::call_and_read_body(&app, req("frontend")).await;
-        let proxy_result = test::call_and_read_body(&app, req("proxy")).await;
-        assert_eq!(frontend_result, proxy_result);
-
-        let expected = FrontendResult {
-            toggles: vec![EvaluatedToggle {
-                name: "test".into(),
-                enabled: true,
-                variant: EvaluatedVariant {
-                    name: "disabled".into(),
-                    enabled: false,
-                    payload: None,
-                },
-                impression_data: false,
-            }],
-        };
-
-        assert_eq!(frontend_result, serde_json::to_vec(&expected).unwrap());
-    }
-
-    #[actix_web::test]
-    #[traced_test]
-    async fn calling_post_requests_resolves_top_level_properties_correctly() {
-        let (feature_cache, token_cache, engine_cache) = build_offline_mode(
-            client_features_with_constraint_requiring_test_property_to_be_42(),
-            vec![
-                "*:development.03fa5f506428fe80ed5640c351c7232e38940814d2923b08f5c05fa7"
-                    .to_string(),
-            ],
-        )
-        .unwrap();
-        let app = test::init_service(
-            App::new()
-                .app_data(Data::from(token_cache))
-                .app_data(Data::from(feature_cache))
-                .app_data(Data::from(engine_cache))
-                .service(web::scope("/api/frontend").service(super::post_frontend_enabled_features))
-                .service(web::scope("/api/proxy").service(super::post_proxy_enabled_features)),
-        )
-        .await;
-
-        let req = |endpoint| test::TestRequest::post()
-            .uri(format!("/api/{endpoint}").as_str())
-            .insert_header(ContentType::json())
-            .insert_header((
-                "Authorization",
-                "*:development.03fa5f506428fe80ed5640c351c7232e38940814d2923b08f5c05fa7",
-            ))
-            .set_json(json!({
-                "test_property": "42"
-            }))
-            .to_request();
-
-        let frontend_result = test::call_and_read_body(&app, req("frontend")).await;
-        let proxy_result = test::call_and_read_body(&app, req("proxy")).await;
-        assert_eq!(frontend_result, proxy_result);
-
-        let expected = FrontendResult {
-            toggles: vec![EvaluatedToggle {
-                name: "test".into(),
-                enabled: true,
-                variant: EvaluatedVariant {
-                    name: "disabled".into(),
-                    enabled: false,
-                    payload: None,
-                },
-                impression_data: false,
-            }],
-        };
-
-        assert_eq!(frontend_result, serde_json::to_vec(&expected).unwrap());
     }
 
     #[actix_web::test]
