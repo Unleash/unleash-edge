9e14e7bb2342e051bd6b045fb290d4ee0028d7b8
Christopher Kolstad <chriswk@getunleash.io>
2025-02-05 16:03:27 +0100

    feat: Edge observability
    Draft state:
    get latency for own endpoints from prometheus
    get latency for upstream endpoints from prometheus
    get process stats from prometheus
    instantiate on startup

---
+++ server/src/http/instance_data.rs
@@ -0,0 +1,47 @@
+use std::sync::{Arc, RwLock};
+
+use prometheus::Registry;
+use tracing::{debug, info, trace};
+
+use crate::metrics::edge_metrics::EdgeInstanceData;
+
+use super::refresher::feature_refresher::FeatureRefresher;
+
+pub async fn send_instance_data(
+    feature_refresher: Arc<FeatureRefresher>,
+    prometheus_registry: &Registry,
+    our_instance_data: Arc<EdgeInstanceData>,
+    downstream_instance_data: Arc<RwLock<Vec<EdgeInstanceData>>>,
+) {
+    loop {
+        trace!("Looping instance data sending");
+        let mut observed_data = our_instance_data.observe(prometheus_registry);
+        {
+            let downstream_instance_data = downstream_instance_data.read().unwrap().clone();
+            for downstream in downstream_instance_data {
+                observed_data = observed_data.add_downstream(downstream);
+            }
+        }
+        {
+            downstream_instance_data.write().unwrap().clear();
+        }
+        let status = feature_refresher
+            .unleash_client
+            .send_instance_data(
+                observed_data,
+                &feature_refresher
+                    .tokens_to_refresh
+                    .iter()
+                    .next()
+                    .map(|t| t.value().clone())
+                    .map(|t| t.token.token.clone())
+                    .expect("No token to refresh, cowardly panic'ing"),
+            )
+            .await;
+        match status {
+            Ok(_) => info!("Posted instance data"),
+            Err(_) => info!("Failed to post instance data"),
+        }
+        tokio::time::sleep(std::time::Duration::from_secs(15)).await;
+    }
+}
