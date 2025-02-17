fc55f0b2f9eed2aa0443d964d57a1b5e71a18e4a
sjaanus <sellinjaanus@gmail.com>
2025-02-17 12:23:59 +0200

    Fix

--- server/src/delta_cache.rs
+++ server/src/delta_cache.rs
@@ -97,119 +97,125 @@ impl DeltaCache {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use unleash_types::client_features::{DeltaEvent, ClientFeature, Segment};
+    use crate::delta_cache::{DeltaCache, DeltaHydrationEvent};
 
-#[test]
-fn test_update_hydration_event_and_remove_event_when_over_limit() {
-    let base_event = DeltaHydrationEvent {
-        event_id: 1,
-        features: vec![
-            ClientFeature {
-                name: "test-flag".to_string(),
-                ..ClientFeature::default()
-            },
-            ClientFeature {
-                name: "my-feature-flag".to_string(),
-                ..ClientFeature::default()
-            },
-        ],
-        segments: vec![
-            Segment { id: 1, constraints: vec![] },
-            Segment { id: 2,constraints: vec![] },
-        ],
-    };
-    let max_length = 2;
-    let mut delta_cache = DeltaCache::new(base_event.clone(), max_length);
+    #[test]
+    fn test_update_hydration_event_and_remove_event_when_over_limit() {
+        let base_event = DeltaHydrationEvent {
+            event_id: 1,
+            features: vec![
+                ClientFeature {
+                    name: "test-flag".to_string(),
+                    ..ClientFeature::default()
+                },
+                ClientFeature {
+                    name: "my-feature-flag".to_string(),
+                    ..ClientFeature::default()
+                },
+            ],
+            segments: vec![
+                Segment { id: 1, constraints: vec![] },
+                Segment { id: 2,constraints: vec![] },
+            ],
+        };
+        let max_length = 2;
+        let mut delta_cache = DeltaCache::new(base_event.clone(), max_length);
 
-    let initial_events = vec![
-        DeltaEvent::FeatureUpdated {
-            event_id: 2,
+        let initial_events = vec![
+            DeltaEvent::FeatureUpdated {
+                event_id: 2,
+                feature: ClientFeature {
+                    name: "my-feature-flag".to_string(),
+                    ..ClientFeature::default()
+                },
+            },
+        ];
+        delta_cache.add_events(initial_events);
+
+        let added_events = vec![
+            DeltaEvent::FeatureUpdated {
+                event_id: 3,
+                feature: ClientFeature {
+                    name: "another-feature-flag".to_string(),
+                    ..ClientFeature::default()
+                },
+            },
+            DeltaEvent::FeatureRemoved {
+                event_id: 4,
+                feature_name: "test-flag".to_string(),
+            },
+            DeltaEvent::SegmentUpdated {
+                event_id: 5,
+                segment: Segment { id: 1,  constraints: vec![] },
+            },
+            DeltaEvent::SegmentRemoved {
+                event_id: 6,
+                segment_id: 2,
+            },
+            DeltaEvent::SegmentUpdated {
+                event_id: 7,
+                segment: Segment { id: 3, constraints: vec![] },
+            },
+        ];
+        delta_cache.add_events(added_events.clone());
+
+        let events: Vec<_> = delta_cache.get_events().to_vec();
+        assert_eq!(events.len(), max_length);
+        assert_eq!(events, added_events[added_events.len() - max_length..]);
+
+        let hydration_event = delta_cache.get_hydration_event();
+        assert_eq!(hydration_event.features.len(), 2);
+        assert_eq!(hydration_event.event_id, 7);
+        assert!(hydration_event.features.iter().any(|f| f.name == "my-feature-flag"));
+        assert!(hydration_event.features.iter().any(|f| f.name == "another-feature-flag"));
+        assert!(hydration_event.segments.iter().any(|s| s.id == 1));
+    }
+
+    #[test]
+    fn test_prevent_mutation_of_previous_feature_updated_events() {
+        let base_event = DeltaHydrationEvent {
+            event_id: 1,
+            features: vec![ClientFeature {
+                name: "base-flag".to_string(),
+                ..ClientFeature::default()
+            }],
+            segments: vec![],
+        };
+        let mut delta_cache = DeltaCache::new(base_event, 10);
+
+        let initial_feature_event = DeltaEvent::FeatureUpdated {
+            event_id: 129,
             feature: ClientFeature {
-                name: "my-feature-flag".to_string(),
+                name: "streaming-test".to_string(),
+                enabled: false,
                 ..ClientFeature::default()
             },
-        },
-    ];
-    delta_cache.add_events(initial_events);
+        };
+        delta_cache.add_events(vec![initial_feature_event.clone()]);
 
-    let added_events = vec![
-        DeltaEvent::FeatureUpdated {
-            event_id: 3,
+        let updated_feature_event = DeltaEvent::FeatureUpdated {
+            event_id: 130,
             feature: ClientFeature {
-                name: "another-feature-flag".to_string(),
+                name: "streaming-test".to_string(),
+                enabled: true,
+                strategies: Some(vec![unleash_types::client_features::Strategy {
+                    name: "new-strategy".into(),
+                    sort_order: None,
+                    segments: None,
+                    variants: None,
+                    constraints: None ,
+                    parameters: None,
+                }]),
                 ..ClientFeature::default()
             },
-        },
-        DeltaEvent::FeatureRemoved {
-            event_id: 4,
-            feature_name: "test-flag".to_string(),
-        },
-        DeltaEvent::SegmentUpdated {
-            event_id: 5,
-            segment: Segment { id: 1,  constraints: vec![] },
-        },
-        DeltaEvent::SegmentRemoved {
-            event_id: 6,
-            segment_id: 2,
-        },
-        DeltaEvent::SegmentUpdated {
-            event_id: 7,
-            segment: Segment { id: 3, constraints: vec![] },
-        },
-    ];
-    delta_cache.add_events(added_events.clone());
+        };
+        delta_cache.add_events(vec![updated_feature_event.clone()]);
 
-    let events: Vec<_> = delta_cache.get_events().to_vec();
-    assert_eq!(events.len(), max_length);
-    assert_eq!(events, added_events[added_events.len() - max_length..]);
+        assert_eq!(delta_cache.get_events()[1], initial_feature_event);
+        assert_eq!(delta_cache.get_events()[2], updated_feature_event);
+    }
 
-    let hydration_event = delta_cache.get_hydration_event();
-    assert_eq!(hydration_event.features.len(), 2);
-    assert_eq!(hydration_event.event_id, 7);
-    assert!(hydration_event.features.iter().any(|f| f.name == "my-feature-flag"));
-    assert!(hydration_event.features.iter().any(|f| f.name == "another-feature-flag"));
-    assert!(hydration_event.segments.iter().any(|s| s.id == 1));
-}
-
-#[test]
-fn test_prevent_mutation_of_previous_feature_updated_events() {
-    let base_event = DeltaHydrationEvent {
-        event_id: 1,
-        features: vec![ClientFeature {
-            name: "base-flag".to_string(),
-            ..ClientFeature::default()
-        }],
-        segments: vec![],
-    };
-    let mut delta_cache = DeltaCache::new(base_event, 10);
-
-    let initial_feature_event = DeltaEvent::FeatureUpdated {
-        event_id: 129,
-        feature: ClientFeature {
-            name: "streaming-test".to_string(),
-            enabled: false,
-            ..ClientFeature::default()
-        },
-    };
-    delta_cache.add_events(vec![initial_feature_event.clone()]);
-
-    let updated_feature_event = DeltaEvent::FeatureUpdated {
-        event_id: 130,
-        feature: ClientFeature {
-            name: "streaming-test".to_string(),
-            enabled: true,
-            strategies: Some(vec![unleash_types::client_features::Strategy {
-                name: "new-strategy".into(),
-                sort_order: None,
-                segments: None,
-                variants: None,
-                constraints: None ,
-                parameters: None,
-            }]),
-            ..ClientFeature::default()
-        },
-    };
-    delta_cache.add_events(vec![updated_feature_event.clone()]);
-
-    assert_eq!(delta_cache.get_events()[1], initial_feature_event);
-    assert_eq!(delta_cache.get_events()[2], updated_feature_event);
 }
