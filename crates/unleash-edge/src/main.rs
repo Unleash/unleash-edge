6f396129d335e59911e256115bf0a7b9b772c711
Simon Hornby <simon@getunleash.io>
2025-08-22 14:31:04 +0200

    feat: shutdown hook (#1118)

--- crates/unleash-edge/src/main.rs
+++ crates/unleash-edge/src/main.rs
@@ -1,13 +1,35 @@
 use clap::Parser;
+use futures::future::join_all;
 use std::net::SocketAddr;
+use std::pin::pin;
+use tokio::signal;
+use tokio::signal::unix::{SignalKind, signal};
 use tower_http::normalize_path::NormalizePathLayer;
 use tracing::info;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
 use unleash_edge::configure_server;
 use unleash_edge_cli::{CliArgs, EdgeMode};
-use unleash_edge_types::EdgeResult;
 use unleash_edge_types::errors::EdgeError;
+use unleash_edge_types::{BackgroundTask, EdgeResult};
+
+async fn shutdown_signal(address: String, shutdown_tasks: Vec<BackgroundTask>) {
+    info!("Edge is listening to http traffic on {}", address);
+
+    let mut sigint = pin!(signal::ctrl_c());
+    let mut sigterm_stream = signal(SignalKind::terminate()).expect("Failed to bind SIGTERM");
+    let mut sigterm = pin!(sigterm_stream.recv());
+
+    tokio::select! {
+        _ = &mut sigint => {
+            info!("Received Ctrl+C (SIGINT), shutting down gracefully...");
+        }
+        _ = &mut sigterm => {
+            info!("Received SIGTERM, shutting down gracefully...");
+        }
+    }
+    join_all(shutdown_tasks).await;
+}
 
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
@@ -33,7 +55,7 @@ async fn main() -> Result<(), anyhow::Error> {
 }
 
 async fn run_server(args: CliArgs) -> EdgeResult<()> {
-    let router = configure_server(args.clone()).await?;
+    let (router, shutdown_tasks) = configure_server(args.clone()).await?;
     let server = router
         .layer(NormalizePathLayer::trim_trailing_slash())
         .into_make_service_with_connect_info::<SocketAddr>();
@@ -49,10 +71,11 @@ async fn run_server(args: CliArgs) -> EdgeResult<()> {
     let http_listener = tokio::net::TcpListener::bind(&args.http.http_server_addr())
         .await
         .map_err(|_| EdgeError::NotReady)?;
-    let _ = axum::serve(http_listener, server.clone()).await;
-    info!(
-        "Edge is listening to http traffic on {}",
-        &args.http.http_server_addr()
-    );
+    let _ = axum::serve(http_listener, server.clone())
+        .with_graceful_shutdown(shutdown_signal(
+            args.http.http_server_addr().clone(),
+            shutdown_tasks,
+        ))
+        .await;
     Ok(())
 }
