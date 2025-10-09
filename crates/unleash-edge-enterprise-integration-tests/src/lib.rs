6db005c9490ce58b61a1e7b9a35407a32277609e
Simon Hornby <liquidwicked64@gmail.com>
2025-10-09 17:31:03 +0200

    wip: janky test for enterprise edge

---
+++ crates/unleash-edge-enterprise-integration-tests/src/lib.rs
@@ -0,0 +1,146 @@
+#[cfg(test)]
+mod tests {
+
+    use axum::Router;
+    use axum::routing::post;
+    use axum_test::TestServer;
+    use chrono::Duration;
+    use std::sync::Arc;
+    use tokio::sync::RwLock;
+    use ulid::Ulid;
+    use unleash_edge::edge_builder::build_edge_state;
+    use unleash_edge_cli::{AuthHeaders, CliArgs, EdgeArgs, HttpServerArgs};
+    use unleash_edge_types::errors::EdgeError;
+    use unleash_edge_types::metrics::instance_data::EdgeInstanceData;
+
+    use unleash_edge_http_client::{ClientMetaInformation, HttpClientArgs, new_reqwest_client};
+
+    use unleash_edge_types::EdgeResult;
+
+    pub async fn heartbeat() -> EdgeResult<()> {
+        Err(EdgeError::Forbidden("This Edge instance is not licensed! The police will arrive at your doorstep imminently".into()))
+    }
+
+    fn build_license_response_router() -> Router {
+        Router::new().route("/edge-licensing/heartbeat", post(heartbeat))
+    }
+
+    async fn test_upstream_server() -> TestServer {
+        let router = Router::new().nest("/api/client", build_license_response_router());
+        TestServer::builder()
+            .http_transport()
+            .build(router)
+            .expect("Failed to build client api test server")
+    }
+
+    fn mock_cli_args() -> CliArgs {
+        CliArgs {
+            http: HttpServerArgs {
+                port: 3063,
+                interface: "127.0.0.1".to_string(),
+                base_path: "".to_string(),
+                tls: unleash_edge_cli::TlsOptions {
+                    tls_enable: false,
+                    tls_server_key: None,
+                    tls_server_cert: None,
+                    tls_server_port: 3043,
+                    redirect_http_to_https: false,
+                },
+                cors: unleash_edge_cli::CorsOptions {
+                    cors_origin: None,
+                    cors_allowed_headers: None,
+                    cors_max_age: 172800,
+                    cors_exposed_headers: None,
+                    cors_methods: None,
+                },
+                allow_list: None,
+                deny_list: None,
+                workers: None,
+            },
+            mode: unleash_edge_cli::EdgeMode::default(),
+            instance_id: "test-instance".to_string(),
+            app_name: "unleash-edge-test".to_string(),
+            markdown_help: false,
+            trust_proxy: unleash_edge_cli::TrustProxy {
+                trust_proxy: false,
+                proxy_trusted_servers: vec![],
+            },
+            disable_all_endpoint: false,
+            edge_request_timeout: 5,
+            edge_keepalive_timeout: 5,
+            log_format: unleash_edge_cli::LogFormat::Plain,
+            auth_headers: unleash_edge_cli::AuthHeaders::default(),
+            token_header: None,
+            internal_backstage: unleash_edge_cli::InternalBackstageArgs {
+                disable_metrics_batch_endpoint: false,
+                disable_metrics_endpoint: false,
+                disable_features_endpoint: false,
+                disable_tokens_endpoint: false,
+                disable_instance_data_endpoint: false,
+            },
+            sentry_config: unleash_edge_cli::SentryConfig {
+                sentry_dsn: None,
+                sentry_tracing_rate: 0.1,
+                sentry_debug: false,
+                sentry_enable_logs: false,
+            },
+            datadog_config: unleash_edge_cli::DatadogConfig { datadog_url: None },
+            otel_config: unleash_edge_cli::OpenTelemetryConfig {
+                otel_collector_url: None,
+            },
+        }
+    }
+
+    #[tokio::test]
+    async fn enterprise_edge_state_shuts_down_process_when_license_is_not_found() {
+        let server = test_upstream_server().await;
+        let client_meta_information = ClientMetaInformation {
+            app_name: "unleash-edge-test".to_string(),
+            connection_id: "test-connection-id".to_string(),
+            instance_id: "test-instance-id".to_string(),
+        };
+
+        let edge_instance_data = Arc::new(EdgeInstanceData::new(
+            "cheese-shop".into(),
+            &Ulid::new(),
+            None,
+        ));
+
+        let http_client = new_reqwest_client(HttpClientArgs {
+            skip_ssl_verification: false,
+            client_identity: None,
+            upstream_certificate_file: None,
+            connect_timeout: Duration::seconds(10),
+            socket_timeout: Duration::seconds(10),
+            keep_alive_timeout: Duration::seconds(10),
+            client_meta_information: client_meta_information.clone(),
+        })
+        .unwrap();
+
+        let cli_args = mock_cli_args();
+        let edge_args = EdgeArgs {
+            upstream_url: server.server_url("/").unwrap().to_string(),
+            tokens: vec!["*:development.hashyhashhash".to_string()],
+            ..EdgeArgs::default()
+        };
+
+        let instances_observed_for_app_context = Arc::new(RwLock::new(vec![]));
+
+        let maybe_edge_state = build_edge_state(
+            cli_args,
+            &edge_args,
+            client_meta_information,
+            edge_instance_data,
+            instances_observed_for_app_context,
+            AuthHeaders::default(),
+            http_client,
+        )
+        .await;
+
+        if let Err(maybe_err) = &maybe_edge_state {
+            println!("Edge state build failed as expected: {:#?}", maybe_err);
+        }
+        //assert that we get an error back from the edge state builder
+        assert!(maybe_edge_state.is_err());
+    }
+}
