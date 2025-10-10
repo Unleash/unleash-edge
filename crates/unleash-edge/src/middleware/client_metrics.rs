85a987b48630dad3ff8cff4c6a27bc323432ca28
Christopher Kolstad <chriswk@getunleash.io>
2025-10-10 12:10:57 +0200

    add sdk_name and version to all three metrics

--- crates/unleash-edge/src/middleware/client_metrics.rs
+++ crates/unleash-edge/src/middleware/client_metrics.rs
@@ -10,13 +10,14 @@ pub const APP_NAME: &str = "app_name";
 pub const SDK_NAME: &str = "sdk_name";
 pub const SDK_VERSION: &str = "sdk_version";
 pub const SPEC_VERSION: &str = "spec_version";
-pub const CONTENT_ENCODING: &str = "content_encoding";
+pub const ACCEPT_ENCODING: &str = "accept_encoding";
+pub const PATH: &str = "path";
 
 pub static CLIENT_SPEC_VERSION: LazyLock<IntCounterVec> = LazyLock::new(|| {
     register_int_counter_vec!(
         "client_spec_version",
         "The client SDK version",
-        &[APP_NAME, INSTANCE_ID, SPEC_VERSION]
+        &[APP_NAME, INSTANCE_ID, SDK_NAME, SDK_VERSION, SPEC_VERSION]
     )
     .unwrap()
 });
@@ -34,7 +35,14 @@ pub static CONTENT_ENCODING_COUNTER: LazyLock<IntCounterVec> = LazyLock::new(|| 
     register_int_counter_vec!(
         "request_content_encoding",
         "The value of the Accept-encoding header",
-        &[APP_NAME, INSTANCE_ID, CONTENT_ENCODING]
+        &[
+            APP_NAME,
+            INSTANCE_ID,
+            ACCEPT_ENCODING,
+            SDK_NAME,
+            SDK_VERSION,
+            PATH
+        ]
     )
     .unwrap()
 });
@@ -44,46 +52,53 @@ pub async fn extract_request_metrics(
     req: Request,
     next: Next,
 ) -> Response {
-    let app_name = state.connect_via.app_name.clone();
-    let instance_id = state.connect_via.instance_id.clone();
+    let uri = req.uri().clone();
+    let path = uri.path();
+    let app_name = state.connect_via.app_name.as_str();
+    let instance_id = state.connect_via.instance_id.as_str();
     let headers = req.headers().clone();
     let res = next.run(req).await;
-    if res.status().is_success() {
-        if let Some(content_encoding) = headers.get(http::header::ACCEPT_ENCODING) {
-            if let Ok(content_encoding) = content_encoding.to_str() {
+    if res.status().is_success() && !path.contains("internal-backstage") {
+        let (sdk_name, version) = match headers.get("unleash-sdk") {
+            Some(sdk_version) => {
+                if let Ok(sdk_version) = sdk_version.to_str() {
+                    sdk_version
+                        .split_once(':')
+                        .unwrap_or((sdk_version, "unknown"))
+                } else {
+                    ("unknown", "unknown")
+                }
+            }
+            None => ("unknown", "unknown"),
+        };
+        if let Some(accept_encoding) = headers.get(http::header::ACCEPT_ENCODING) {
+            if let Ok(accept_encoding) = accept_encoding.to_str() {
                 CONTENT_ENCODING_COUNTER
                     .with_label_values(&[
-                        &app_name.as_str(),
-                        &instance_id.as_str(),
-                        &content_encoding,
+                        &app_name,
+                        &instance_id,
+                        &accept_encoding,
+                        &sdk_name,
+                        &version,
+                        &path,
                     ])
                     .inc()
             }
         } else {
             CONTENT_ENCODING_COUNTER
-                .with_label_values(&[&app_name.as_str(), &instance_id.as_str(), &"none"])
+                .with_label_values(&[&app_name, &instance_id, &"none", &sdk_name, &version, &path])
                 .inc()
         }
-        if let Some(sdk_version) = headers.get("unleash-sdk")
-            && let Ok(sdk_version) = sdk_version.to_str()
-        {
-            let (sdk_name, version) = sdk_version
-                .split_once(':')
-                .unwrap_or((sdk_version, "unknown"));
-            CLIENT_SDK_VERSION
-                .with_label_values(&[
-                    &app_name.as_str(),
-                    &instance_id.as_str(),
-                    &sdk_name,
-                    &version,
-                ])
-                .inc();
-        }
+
+        CLIENT_SDK_VERSION
+            .with_label_values(&[&app_name, &instance_id, &sdk_name, &version])
+            .inc();
+
         if let Some(client_spec) = headers.get("unleash-client-spec")
             && let Ok(client_spec) = client_spec.to_str()
         {
             CLIENT_SPEC_VERSION
-                .with_label_values(&[&app_name.as_str(), &instance_id.as_str(), &client_spec])
+                .with_label_values(&[&app_name, &instance_id, &sdk_name, &version, &client_spec])
                 .inc();
         }
     }
