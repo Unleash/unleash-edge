2fb8d7b4289d868fd83bcd34ffbb5afa10657533
Christopher Kolstad <chriswk@getunleash.io>
2025-11-05 10:25:16 +0100

    steal tests from towers NormalizePath middleware

--- crates/oss/unleash-edge/src/middleware/trim_multiple_and_trailing_slashes.rs
+++ crates/oss/unleash-edge/src/middleware/trim_multiple_and_trailing_slashes.rs
@@ -92,4 +92,99 @@ mod tests {
             .into_body();
         assert_eq!(body, "/foo/bar");
     }
+
+    #[tokio::test]
+    async fn trims_all_amount_of_slashes() {
+        async fn handle(request: Request<()>) -> Result<Response<String>, Infallible> {
+            Ok(Response::new(request.uri().to_string()))
+        }
+
+        let mut svc = ServiceBuilder::new()
+            .layer(NormalizePathFullLayer)
+            .service_fn(handle);
+        let body = svc
+            .ready()
+            .await
+            .unwrap()
+            .call(
+                Request::builder()
+                    .uri("/////foo////////////bar//////")
+                    .body(())
+                    .unwrap(),
+            )
+            .await
+            .unwrap()
+            .into_body();
+        assert_eq!(body, "/foo/bar");
+    }
+
+    #[test]
+    fn is_noop_if_no_trailing_slash() {
+        let mut uri = "/foo".parse::<Uri>().unwrap();
+        trim_trailing_and_double_slashes(&mut uri);
+        assert_eq!(uri, "/foo");
+    }
+
+    #[test]
+    fn maintains_query() {
+        let mut uri = "/foo/?a=a".parse::<Uri>().unwrap();
+        trim_trailing_and_double_slashes(&mut uri);
+        assert_eq!(uri, "/foo?a=a");
+    }
+
+    #[test]
+    fn removes_multiple_trailing_slashes() {
+        let mut uri = "/foo////".parse::<Uri>().unwrap();
+        trim_trailing_and_double_slashes(&mut uri);
+        assert_eq!(uri, "/foo");
+    }
+
+    #[test]
+    fn removes_multiple_trailing_slashes_even_with_query() {
+        let mut uri = "/foo////?a=a".parse::<Uri>().unwrap();
+        trim_trailing_and_double_slashes(&mut uri);
+        assert_eq!(uri, "/foo?a=a");
+    }
+
+    #[test]
+    fn is_noop_on_index() {
+        let mut uri = "/".parse::<Uri>().unwrap();
+        trim_trailing_and_double_slashes(&mut uri);
+        assert_eq!(uri, "/");
+    }
+
+    #[test]
+    fn removes_multiple_trailing_slashes_on_index() {
+        let mut uri = "////".parse::<Uri>().unwrap();
+        trim_trailing_and_double_slashes(&mut uri);
+        assert_eq!(uri, "/");
+    }
+
+    #[test]
+    fn removes_multiple_trailing_slashes_on_index_even_with_query() {
+        let mut uri = "////?a=a".parse::<Uri>().unwrap();
+        trim_trailing_and_double_slashes(&mut uri);
+        assert_eq!(uri, "/?a=a");
+    }
+
+    #[test]
+    fn removes_multiple_slashes_in_the_middle_of_path() {
+        let mut uri = "/foo//////bar////baz////".parse::<Uri>().unwrap();
+        trim_trailing_and_double_slashes(&mut uri);
+        assert_eq!(uri, "/foo/bar/baz");
+    }
+
+    #[test]
+    fn removes_multiple_preceding_slashes_even_with_query() {
+        let mut uri = "///foo//?a=a".parse::<Uri>().unwrap();
+        trim_trailing_and_double_slashes(&mut uri);
+        assert_eq!(uri, "/foo?a=a");
+    }
+
+    #[test]
+    fn removes_multiple_preceding_slashes() {
+        let mut uri = "///foo".parse::<Uri>().unwrap();
+        trim_trailing_and_double_slashes(&mut uri);
+        assert_eq!(uri, "/foo");
+    }
 }
