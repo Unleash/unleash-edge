2268aac59158564d02ece8b8cf99e525d45ec276
Simon Hornby <liquidwicked64@gmail.com>
2026-09-02 08:52:07 +0200

    docs: simple enricher example

---
+++ examples/context-enrichers/simple-enricher.js
@@ -0,0 +1,9 @@
+module.exports = async function enrich(context, headers) {
+  return {
+    ...context,
+    userId: headers["x-user-id"] || context.userId,
+    properties: {
+      ...(context.properties || {})
+    },
+  };
+};
