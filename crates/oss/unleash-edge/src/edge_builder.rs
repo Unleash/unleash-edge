fc05bb3f50bc08b463015873b8df4971414542a0
Christopher Kolstad <chriswk@getunleash.io>
2026-09-23 10:46:24 +0200

    also remove features cached for other envs

--- crates/oss/unleash-edge/src/edge_builder.rs
+++ crates/oss/unleash-edge/src/edge_builder.rs
@@ -330,6 +330,7 @@ pub async fn build_edge(
         }
     }
     hydrator_type.hydrate_new_tokens().await;
+    feature_cache.keep_wanted_environments(desired_envs);
     Ok((
         (token_cache, feature_cache, delta_cache, engine_cache),
         Arc::new(token_validator),
